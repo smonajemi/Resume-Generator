d16e61a6d963a939684c2e0f02469ffb5f615651
Sina Monajemi <59583555+smonajemi@users.noreply.github.com>
2023-02-07 13:42:13 -0500

    commit

---
+++ server.js
@@ -0,0 +1,21 @@
+
+const express = require('express')
+const path = require("path");
+const app = express()
+
+// #############################################################################
+var options = {
+  dotfiles: 'ignore',
+  etag: false,
+  extensions: ['htm', 'html','css','js','ico','jpg','jpeg','png','svg'],
+  index: ['index.html'],
+  maxAge: '1m',
+  redirect: false
+}
+app.use(express.static('build', options))
+
+const port = process.env.PORT || 4000
+
+app.listen(port, () => {
+  console.log(`React app listening at http://localhost:${port}`)
+})
