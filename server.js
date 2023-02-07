08782e6d4fd7df3af4290029f0ae23c86c267e67
Sina Monajemi <59583555+smonajemi@users.noreply.github.com>
2023-02-07 14:15:10 -0500

    commit

--- server.js
+++ server.js
@@ -1,21 +1,12 @@
-
-const express = require('express')
-const path = require("path");
-const app = express()
-
-app.use(express.static(path.join(__dirname, "./public/build")))
-app.get("*", (_, res) => {
-    res.sendFile(
-        path.join(__dirname, "./public/build/index.html"),
-        function (error) {
-            res.status(500).send(error)
-            
-        }
-    )
-})
-
-const port = process.env.PORT || 4000
-
+const path = require('path');
+const express = require('express');
+const app = express();
+const publicPath = path.join(__dirname, '..', 'public');
+const port = process.env.PORT || 4000;
+app.use(express.static(publicPath));
+app.get('*', (req, res) => {
+   res.sendFile(path.join(publicPath, 'index.html'));
+});
 app.listen(port, () => {
-  console.log(`React app listening at http://localhost:${port}`)
-})
+   console.log('Server is up!');
+});
