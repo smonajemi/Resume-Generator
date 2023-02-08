4ea2320688c922697a36a4a7a5518d4817606b0f
Sina Monajemi <59583555+smonajemi@users.noreply.github.com>
2023-02-07 21:12:45 -0500

    commit

--- src/components/ErrorToaster.tsx
+++ src/components/ErrorToaster.tsx
@@ -1,6 +1,5 @@
 import { FunctionComponent } from 'react';
-import { Box, Snackbar } from '@mui/material';
-import { makeStyles } from '@material-ui/core/styles';
+import { Box, makeStyles, Snackbar } from '@mui/material';
 
 const useStyles = makeStyles((theme: any) => ({
   root: {
