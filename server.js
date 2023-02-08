8dfe6a0f1c1569a3ee719fd5ee2260146f1fcbaf
Sina Monajemi <59583555+smonajemi@users.noreply.github.com>
2023-02-07 21:31:20 -0500

    commit

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 9000;
 
-app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static(path.join(__dirname, 'public/build')));
 
 app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+  res.sendFile(path.join(__dirname, 'public/build', 'index.html'));
 });
 
 app.listen(port, () => {
