fd6eca71052ccc33dbae277d5ddebde957a855d8
Sina Monajemi <59583555+smonajemi@users.noreply.github.com>
2023-03-01 07:53:18 -0500

    commit

--- server.js
+++ server.js
@@ -9,13 +9,13 @@ app.use(express.json())
 app.use(cors());
 app.use(express.static(path.join(__dirname, 'build')));
 
-app.get('/build', function (req, res) {
+app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
-app.get('/', (req, res) => {
-  res.send('API is working...')
-})
+// app.get('/', (req, res) => {
+//   res.send('API is working...')
+// })
 
 app.post('/api/:apiName', async (req, res) => {
   const param = req.params.apiName
