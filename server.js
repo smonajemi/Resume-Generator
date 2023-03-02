25e22fd92e3f776d77b84d91b149150c78659ab8
Sina Monajemi <59583555+smonajemi@users.noreply.github.com>
2023-03-01 19:42:28 -0500

    commit

--- server.js
+++ server.js
@@ -1,17 +1,17 @@
-const express = require('express');
-const path = require('path');
-const app = express();
-const port = process.env.REACT_APP_PORT || 9000;
-const cors = require('cors');
+const express = require('express')
+const path = require('path')
+const app = express()
+const port = process.env.REACT_APP_PORT || 9000
+const cors = require('cors')
 
 app.use(express.json())
-app.use(cors());
-app.use(express.static(path.join(__dirname, 'build')));
+app.use(cors())
+app.use(express.static(path.join(__dirname, 'build')))
 
-app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'))
+})
 
 app.listen(port, () => {
-   console.log(`Server is running on port: ${port}`);
-});
+   console.log(`Server is running on port: ${port}`)
+})
