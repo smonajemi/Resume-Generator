a51abab24a1691fadf14e336c5cf2be1f858644f
Sina Monajemi <59583555+smonajemi@users.noreply.github.com>
2023-04-12 03:37:14 -0400

    commit

--- src/config/AuthCheker.tsx
+++ src/config/AuthCheker.tsx
@@ -1,20 +1,37 @@
-import React, { useEffect } from "react";
-import { useNavigate } from "react-router-dom";
-import { useLocalStorage } from "../components/hooks/useLocalStorage";
+import React, { useEffect, useState, ReactNode } from "react";
+import { useNavigate, useLocation } from "react-router-dom";
+import { useAuth } from "../components/hooks/useAuth";
+import { CircularProgress, Backdrop } from "@mui/material";
+
 interface Props {
-  children: React.ReactNode;
+  children: ReactNode;
 }
 
 const AuthChecker = ({ children }: Props) => {
   const navigate = useNavigate();
-  const {getItem} = useLocalStorage()
-  useEffect(() => {
-    if (!getItem('userId')) {
-      navigate("/login");
-    } else navigate('/')
-  }, []); // eslint-disable-line react-hooks/exhaustive-deps
+  const { isAuthenticated } = useAuth();
+  const { pathname } = useLocation();
+  const [isNavigating, setIsNavigating] = useState(false);
 
-  return <>{children}</>;
+  useEffect(() => {
+    const navigateToLoginPage = async () => {
+      if (isAuthenticated && (pathname === "/login" || pathname === "/signup")) {
+        setIsNavigating(true);
+        await navigate(-1);
+        setIsNavigating(false);
+      }
+    };
+    navigateToLoginPage();
+  }, [isAuthenticated, pathname, navigate]);
+
+  return (
+    <>
+      <Backdrop open={isNavigating} sx={{ color: "#fff", zIndex: (theme) => theme.zIndex.drawer + 1 }}>
+        <CircularProgress />
+      </Backdrop>
+      {children}
+    </>
+  );
 };
 
 export default AuthChecker;
