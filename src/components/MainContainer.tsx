5183314643457997cce87d4795055bf36381ea38
Sina Monajemi <59583555+smonajemi@users.noreply.github.com>
2023-04-12 10:47:59 -0400

    commit

--- src/components/MainContainer.tsx
+++ src/components/MainContainer.tsx
@@ -16,7 +16,7 @@ export const MainContainer: FunctionComponent<IMainContainerProps> = ({ title, c
         <Box style={{ width: '100%', height: '60px', position: 'fixed', top: 0, left: 0, right: 0 }}>
           <Navbar />
         </Box>
-        <Box style={{ width: '100%', height: 'auto', display: 'flex', justifyContent: 'center', alignItems: 'center', overflow: 'auto', marginTop: '2em' }}>
+        <Box style={{ width: '100%', height: 'auto', display: 'flex', justifyContent: 'center', alignItems: 'center', overflow: 'auto', marginTop: '3em' }}>
           {children}
         </Box>
       </Box>
