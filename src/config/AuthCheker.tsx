4603da9f5178ca8920009a7c6a16f6833e3efdea
Sina Monajemi <59583555+smonajemi@users.noreply.github.com>
2023-04-12 13:13:09 -0400

    commit

--- src/config/AuthCheker.tsx
+++ src/config/AuthCheker.tsx
@@ -16,22 +16,35 @@ const AuthChecker = ({ children }: Props) => {
   const { pathname } = useLocation();
   const [isNavigating, setIsNavigating] = useState(true);
   const [isOpen, setOpen] = useState(false);
+  const [toasterMessage, setToasterMessage] = useState({ severity: '', message: '' });
 
   useEffect(() => {
     const navigateToLoginPage = async () => {
-      const userId = getItem('userId');
-      if (!userId) {
-        setIsNavigating(true);
-        await navigate("/login");
-      } else if (pathname === "/profile" && userId?.includes('guest')) {
-        setOpen(true)
-        await navigate('/');
-        setIsNavigating(true);
-      } else if (pathname === "/login" || pathname === "/signup") {
-        setIsNavigating(true);
-        await navigate(-1);
+      try {
+        const userId = getItem('userId');
+        if (!userId) {
+          setIsNavigating(true);
+          await navigate("/login");
+        } else if (pathname === "/profile" && userId?.includes('guest')) {
+          await navigate('/');
+          setToasterMessage({ ...toasterMessage, message: "You're almost toast-worthy! But first, please sign up to access this page", severity: 'warning' });
+          setOpen(true);
+          setIsNavigating(false);
+        } else if (pathname === "/login" || pathname === "/signup") {
+          setIsNavigating(true);
+          await navigate(-1);
+          setIsNavigating(false);
+        }
+      } catch (error) {
+        if (typeof error === "string") {
+          setToasterMessage({ ...toasterMessage, message: error, severity: 'error' });
+        } else if (error instanceof Error) {
+          setToasterMessage({ ...toasterMessage, message: error.message, severity: 'error' });
+        } else {
+          setToasterMessage({ ...toasterMessage, message: 'An error occurred.', severity: 'error' });
+        }
+        setOpen(true);
       }
-      // setIsNavigating(false);
     }
 
     navigateToLoginPage();
@@ -41,12 +54,13 @@ const AuthChecker = ({ children }: Props) => {
     setIsNavigating(false); // Set to false after navigation is complete
   }, [pathname]);
 
+
   return (
     <>
       <Backdrop open={isNavigating} sx={{ color: "#fff", zIndex: (theme) => theme.zIndex.drawer + 1 }}>
         <CircularProgress />
       </Backdrop>
-      <DefaultToaster setOpen={setOpen} isOpen={isOpen} severity={"warning"} message={"You're almost toast-worthy! But first, please sign up to access this page"} />
+      <DefaultToaster setOpen={setOpen} isOpen={isOpen} severity={toasterMessage?.severity as any} message={toasterMessage?.message} />
       {children}
     </>
   );
