6e7aecae2d86f06472a4cd58451563922ea6320f
Sina Monajemi <59583555+smonajemi@users.noreply.github.com>
2023-04-26 15:12:12 -0400

    commit

--- src/pages/Questionnaire.tsx
+++ src/pages/Questionnaire.tsx
@@ -28,8 +28,10 @@ const useStyles = makeStyles({
     marginBottom: "10px",
   },
   widerCard: {
+    overflow: 'auto',
+    height: '45em',
     width: "80%", // Adjust this value to change the width of the card
-    marginTop: '2em',
+    marginTop: "2em",
     display: "flex",
     flexDirection: "column",
     alignItems: "center",
@@ -83,7 +85,6 @@ interface ChatMessage {
   isUser: boolean;
 }
 
-
 const Questionnaire: React.FC = () => {
   const classes = useStyles();
   const [inputValue, setInputValue] = useState("");
@@ -126,76 +127,123 @@ const Questionnaire: React.FC = () => {
 
   return (
     <MainContainer title={"Questionnaire"}>
-      <Card className={classes.widerCard}>
-        <CardContent>
-          <Box display="flex" flexDirection="column" marginTop="1em">
+      <Card className={classes.widerCard} >
+        <CardContent >
+          <Box display="flex" flexDirection="column" marginTop="1em" style={{}}>
             {chatHistory.map((chatMessage, index) => (
               <Fragment key={index}>
-                 <Box display="flex" justifyContent={chatMessage.isUser ? "flex-end" : "flex-start"} style={{ padding: "0.5em .5em 0 0.5em",marginTop: ".5em",
-maxWidth: "75%", borderWidth: "2px", backgroundColor: "yellow",borderStyle: "solid", borderColor: 'red', borderRadius: "20px", width: 'fit-content', marginLeft: 'auto'}}>
+                <Box
+                  display="flex"
+                  justifyContent={
+                    chatMessage.isUser ? "flex-end" : "flex-start"
+                  }
+                  style={{
+                    padding: "0.5em .5em 0em 0.5em",
+                    marginTop: ".5em",
+                    maxWidth: "75%",
+                    borderWidth: "2px",
+                    backgroundColor: "yellow",
+                    borderStyle: "solid",
+                    borderColor: "red",
+                    borderRadius: "20px",
+                    width: "fit-content",
+                    marginLeft: "auto",
+                  }}
+                >
                   <Typography
                     variant="body1"
-                    className={`${classes.chatMessage} ${chatMessage.isUser ? "user" : "bot"}`}
-                    style={{  textAlign: 'right' }}
+                    className={`${classes.chatMessage} ${
+                      chatMessage.isUser ? "user" : "bot"
+                    }`}
+                    style={{ textAlign: "right" }}
                   >
                     {chatMessage.question}
                   </Typography>
                 </Box>
                 {chatMessage.message ? (
-                           <Box display="flex" justifyContent={chatMessage.isUser ? "flex-end" : "flex-start"}  style={{ padding: "0.5em .5em 0 0.5em", marginTop:".5em",
-                           maxWidth: "75%", borderWidth: "2px", backgroundColor: "red",borderStyle: "solid", borderColor: 'yellow', borderRadius: "20px", width: 'fit-content', marginRight: 'auto'}}>
-                                             <Typography
-                                               variant="body1"
-                                               className={`${classes.chatMessage} ${chatMessage.isUser ? "user" : "bot"}`}
-                                               style={{ textAlign: 'left', wordWrap: 'break-word' }}
-                                             >
-                                               {chatMessage.message}
-                                               
-                                             </Typography>
-                                           </Box>
+                  <Box
+                    display="flex"
+                    justifyContent={
+                      chatMessage.isUser ? "flex-end" : "flex-start"
+                    }
+                    style={{
+                      padding: "0.5em .5em 0em 0.5em",
+                      marginTop: ".5em",
+                      maxWidth: "75%",
+                      borderWidth: "2px",
+                      backgroundColor: "red",
+                      borderStyle: "solid",
+                      borderColor: "yellow",
+                      borderRadius: "20px",
+                      width: "fit-content",
+                      marginRight: "auto",
+                    }}
+                  >
+                    <Typography
+                      variant="body1"
+                      className={`${classes.chatMessage} ${
+                        chatMessage.isUser ? "user" : "bot"
+                      }`}
+                      style={{ textAlign: "left", wordWrap: "break-word" }}
+                    >
+                      {chatMessage.message}
+                    </Typography>
+                  </Box>
                 ) : (
-                  <Box display="flex" justifyContent={chatMessage.isUser ? "flex-end" : "flex-start"} marginBottom="0.5em" style={{ padding: "0.5em .5em 0 0.5em", margin:".5em 0 .5rm 0",
-                  maxWidth: "75%", borderRadius: "20px", width: 'fit-content', marginRight: 'auto'}}>
-                                    <Typography
-                                      variant="body1"
-                                      className={`${classes.chatMessage} ${chatMessage.isUser ? "user" : "bot"}`}
-                                      style={{ textAlign: 'left', wordWrap: 'break-word' }}
-                                    >
-                                  <Box className={classes.root}>
-          <Box className={classes.dotOne}></Box>
-          <Box className={classes.dotTwo}></Box>
-          <Box className={classes.dotThree}></Box>
-        </Box>
-                                      
-                                    </Typography>
-                                  </Box>
+                  <Box
+                    display="flex"
+                    justifyContent={
+                      chatMessage.isUser ? "flex-end" : "flex-start"
+                    }
+                    marginBottom="0.5em"
+                    style={{
+                      padding: "0.5em .5em 0em 0.5em",
+                      margin: ".5em 0 .5rm 0",
+                      maxWidth: "75%",
+                      borderRadius: "20px",
+                      width: "fit-content",
+                      marginRight: "auto",
+                    }}
+                  >
+                    <Typography
+                      variant="body1"
+                      className={`${classes.chatMessage} ${
+                        chatMessage.isUser ? "user" : "bot"
+                      }`}
+                      style={{ textAlign: "left", wordWrap: "break-word" }}
+                    >
+                      <Box className={classes.root}>
+                        <Box className={classes.dotOne}></Box>
+                        <Box className={classes.dotTwo}></Box>
+                        <Box className={classes.dotThree}></Box>
+                      </Box>
+                    </Typography>
+                  </Box>
                 )}
               </Fragment>
             ))}
           </Box>
-  
-          <Box style={{display: 'flex', marginTop: '1em'}}>
-            <Box flex="1">
-              <TextField
-                id="chat-input"
-                label="Type your question here"
-                value={inputValue}
-                onChange={handleInputValueChange}
-                variant="outlined"
-              />
-            </Box>
-  
-            <Box style={{ padding: ".5em" }}>
-              <Button variant="contained" color="primary" onClick={handleSendClick}>
-                Send
-              </Button>
-            </Box>
-          </Box>
+          <Box style={{display: 'flex', justifyContent: 'center', marginTop: '1em', position: 'fixed', bottom: '1em', left: 0, right: 0}}>
+  <Box flex="1">
+    <TextField
+      id="chat-input"
+      label="Type your question here"
+      value={inputValue}
+      onChange={handleInputValueChange}
+      variant="outlined"
+    />
+  </Box>
+
+  <Box style={{ padding: ".5em" }}>
+    <Button variant="contained" color="primary" onClick={handleSendClick}>
+      Send
+    </Button>
+  </Box>
+</Box>
         </CardContent>
       </Card>
     </MainContainer>
   );
-  
 };
 
 export default Questionnaire;
